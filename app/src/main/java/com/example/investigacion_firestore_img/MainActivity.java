76f0afe8df9a2928267bb1f098f95c60e5df69a9
Raul Ramirez <raul.ramirez@escuelaestech.es>
2019-10-29 20:40:20 +0100

    Cargada imagen de la base de datos

--- app/src/main/java/com/example/investigacion_firestore_img/MainActivity.java
+++ app/src/main/java/com/example/investigacion_firestore_img/MainActivity.java
@@ -1,42 +1,44 @@
 package com.example.investigacion_firestore_img;
 
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore mFirestore;
     FirebaseStorage mStorage;
-    ImageView imageView;
+    FirebaseAuth mAuth;
+    ImageView mImage;
+
+    final long ONE_MEGABYTE = 1024 * 1024;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         mFirestore = FirebaseFirestore.getInstance();
         mStorage = FirebaseStorage.getInstance();
-        imageView = findViewById(R.id.image);
+        //Esto lo hice porque necesitaba autenticarme para consultar las imágenes, no es necesario realizarlo en el proyecto.
+        mAuth = FirebaseAuth.getInstance();
+        mImage = findViewById(R.id.mImage);
+
+        mAuth.signInAnonymously();
 
         readData();
-
-        setContentView(R.layout.activity_main);
     }
 
     private void readData(){
@@ -44,9 +46,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()){
-                    QuerySnapshot document = task.getResult();
                     String image = task.getResult().getDocuments().get(0).getString("image");
-                    Log.e("IMAGE", image);
+                    mStorage.getReference().child(image).getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                            mImage.setImageBitmap(Bitmap.createScaledBitmap(bmp, mImage.getWidth(), mImage.getHeight(), false));
+                        }
+                    }).addOnCanceledListener(new OnCanceledListener() {
+                        @Override
+                        public void onCanceled() {
+                            Log.e("DOWNLOAD", "ERROR");
+                        }
+                    });
                 }
             }
         });
